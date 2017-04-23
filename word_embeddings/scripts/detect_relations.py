72dc0ed0bf43763a105e07480c6b603bbd27998b
Konstantin.Agapov <konstagapov@ya.ru>
2017-04-24 01:06:21 +0300

    Added classifier based on SVM to find out whether a set of word pairs has the same relation between words in each pair

--- word_embeddings/scripts/detect_relations.py
+++ word_embeddings/scripts/detect_relations.py
@@ -1,21 +1,62 @@
 import sys
 from embeddings import Vocab, WordEmbedding
+from numpy import linalg as la
 import numpy as np
+from sklearn.svm import LinearSVC
+
+
+def cosine(x, y):
+    # Cosine of angle between vectors x and y
+    return x.dot(y) / (la.norm(x) * la.norm(y))
+
 
 def load_example_sets(path):
     # Loads list of pairs per line.
-    return [[tuple(pair.split()) for pair in line.strip().split('\t')] for line in open(path)]
+    return [[tuple(pair.split())
+             for pair in line.strip().split('\t')]
+            for line in open(path)]
+
 
 def load_labels(path):
-    # Loads a label for each line (-1 indicates the pairs do not form a relation).
+    # Loads a label for each line
+    # (-1 indicates the pairs do not form a relation).
     return [int(label) for label in open(path)]
 
-def label_all_examples_as_not_a_relation(examples):
+
+def label_all_examples_as_not_a_relation(examples, embedding):
     return [-1 for example in examples]
+
+
+def extractFeatures(sample, embedding):
+    embeddingDiffs = np.array(
+        [embedding.Projection(word1) - embedding.Projection(word2)
+         for word1, word2 in sample]
+    )
+    meanDiff = np.mean(embeddingDiffs, axis=0)
+    avgCosineDiff = max([cosine(diff, meanDiff)
+                         for diff in embeddingDiffs]) / len(embeddingDiffs)
+
+    return np.concatenate([
+        meanDiff,
+        np.std(embeddingDiffs, axis=0),
+        [avgCosineDiff]]
+    )
+
+
+def classifyByLogisticRegression(trainData, trainLabels, examples, embedding):
+    estimator = LinearSVC()
+    trainFeatures = np.array([extractFeatures(sample, embedding)
+                              for sample in trainData])
+    estimator.fit(trainFeatures, trainLabels)
+    testFeatures = np.array([extractFeatures(sample, embedding)
+                             for sample in examples])
+    return estimator.predict(testFeatures)
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 6:
-        print "Usage ./detect_relations.py vocab_file embedding_file train_data train_labels test_data"
+        print("Usage ./detect_relations.py vocab_file embedding_file"
+              " train_data train_labels test_data")
         sys.exit(0)
 
     # Load vocab and embedding (these are not used yet!)
@@ -25,21 +66,16 @@ if __name__ == '__main__':
     # Loads training data and labels.
     training_examples = load_example_sets(sys.argv[3])
     training_labels = load_labels(sys.argv[4])
-    assert len(training_examples) == len(training_labels), "Expected one label for each line in training data."
+    assert len(training_examples) == len(
+        training_labels), "Expected one label for each line in training data."
 
     # Load test examples and labels each set of pairs as 'not a relation' (-1)
     # This is not a good idea... You can definitely do better!
     test_examples = load_example_sets(sys.argv[5])
-    for null_label in label_all_examples_as_not_a_relation(test_examples):
-        print '%d' % null_label
+    # for null_label in label_all_examples_as_not_a_relation(test_examples):
+    #    print('%d' % null_label)
 
-    # TODO
-    # 1. Try using the word embedding to discriminate between sets of pairs
-    # that belong to a common relation and ones that don't (labelled -1).
-    # E.g. training_example = [ (dog, dogs), (cat, cats), ... ], training_label = 3
-    #      training_example = [ (dog, fishing), (cats, in), ...], training_label = -1
-    #
-    # 2. Once you have a model that can discriminate related pairs from unrelated ones,
-    # try to further classify those that form a relation into a specific class. 
-    # Note: all relations (labels) are observed at least one in the training data.
-
+    for label in classifyByLogisticRegression(
+        training_examples, training_labels, test_examples, embedding
+    ):
+        print('%d' % label)
